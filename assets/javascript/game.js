7948df67954065348aae8f49b792a59c1d53668a
Brice Randolph <br042393@gmail.com>
2017-07-25 14:33:52 -0700

    Initial Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+/*
+
+
+ Hangman Game
+
+
+
+*/
+window.onload = function() {
+    // all of your code goes in here
+    // it runs after the DOM is built
+
+    console.log("HEYYYY");
+
+    document.getElementById("userInput").innerHTML = "You choose:";
+
+}
